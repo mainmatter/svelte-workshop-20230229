6c635045cd5a7c8ba6cd5573fd60f30e3551d362
paoloricciuti <ricciutipaolo@gmail.com>
2024-02-29 10:45:37 +0100

    basic todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let todos = [
+		{
+			completed: true,
+			content: 'Learn HTML',
+			id: '1'
+		},
+		{
+			completed: false,
+			content: 'Learn CSS',
+			id: '2'
+		}
+	];
+
+	$: completed_items = todos.filter((todo) => todo.completed).length;
+
+	let new_todo = '';
+</script>
+
+<input bind:value={new_todo} />
+<button
+	on:click={() => {
+		todos.push({
+			completed: false,
+			id: crypto.randomUUID(),
+			content: new_todo
+		});
+		todos = todos;
+		new_todo = '';
+	}}>Add todo</button
+>
+Completed items: {completed_items}
+
+{#each todos as todo (todo.id)}
+	<button
+		class:completed={todo.completed}
+		on:click={() => {
+			todo.completed = !todo.completed;
+			todos = todos;
+		}}
+	>
+		{todo.content} - {todo.completed}
+	</button>
+{/each}
+
+<style>
+	.completed {
+		text-decoration: line-through;
+	}
+	button {
+		all: unset;
+		display: block;
+	}
+</style>
